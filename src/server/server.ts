be996098066a5ac59d92680f886ca77310f4c4a0
David Rivera <drtaupier@gmail.com>
2022-10-10 12:38:18 -0400

    Se creo el archivo para manejar el servidor

---
+++ src/server/server.ts
@@ -0,0 +1,28 @@
+import express, {Request, Response} from 'express';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+import userRoutes from './handlers/users'
+import messageRoutes from './handlers/message';
+const path = require('path');
+const port = process.env.PORT;
+const app: express.Application = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(morgan('dev'));
+
+app.use(express.static(path.resolve(__dirname, "../../public")));
+app.use(express.static(__dirname));
+
+app.get('/', (_req: Request, res: Response) => {
+	res.sendFile(__dirname + "/index.html")
+});
+
+userRoutes(app);
+messageRoutes(app);
+
+app.listen(port, () => {
+	console.log(`Server started at localhost ${port}`);
+});
+
+export default app;
