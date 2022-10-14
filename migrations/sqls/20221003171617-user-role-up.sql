8985fc6289ee009450ef77951759d7f3102a2ae7
David Rivera <drtaupier@gmail.com>
2022-10-13 21:34:39 -0400

    Se eliminó el archivo para generar una nueva migración con nuevas relaciones

--- migrations/sqls/20221003171617-user-role-up.sql
+++
@@ -1,6 +0,0 @@
-DROP TABLE IF EXISTS user_role;
-DROP SEQUENCE IF EXISTS user_role__role_id_seq;
-CREATE TABLE user_role(role_id SERIAL PRIMARY KEY, user_role VARCHAR(80) NOT NULL);
-
-INSERT INTO user_role(user_role) VALUES ('ADMIN');
-INSERT INTO user_role(user_role) VALUES ('USER');
