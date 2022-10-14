632fbea09ad5016a074dd54ef8936b441eedf9d4
David Rivera <drtaupier@gmail.com>
2022-10-13 21:39:34 -0400

    Se creo la relación message-users

---
+++ src/server/handlers/message_users.ts
@@ -0,0 +1,111 @@
+import express, { Request, Response } from 'express';
+import verifyAuthToken from '../middlewares/auth';
+import { Message_users, Message_usersStore} from '../models/message_users'
+
+const store = new Message_usersStore();
+
+const index = async (_req: Request, res: Response)=>{
+    try {
+        const message_users = await store.index();
+        res.status(200).json(message_users);
+    } catch (error) {
+        res.status(400).json(error)
+    }
+}
+
+const show = async (req: Request, res: Response) => {
+    try {
+        const message_user = await store.show(req.params.message_users_id)
+        res.status(200).json(message_user);
+    } catch (error) {
+        res.status(400).json(error)
+    }
+}
+
+const showFirstAttempt = async (req: Request, res: Response) => {
+    try {
+    const message = await store.showFirstAttempt();
+        res.status(200).json(message);
+    } catch (error){
+        res.status(400).json(error);
+    }
+}
+
+const showSecondAttempt = async (req: Request, res: Response) => {
+    try {
+    const message = await store.showSecondAttempt();
+        res.status(200).json(message);
+    } catch (error){
+        res.status(400).json(error);
+    }
+}
+
+const showInProcess = async (req: Request, res: Response) => {
+    try {
+        const message = await store.showInProcess();
+        res.status(200).json(message);
+    } catch (error){
+        res.status(400).json(error);
+    }
+}
+
+const showAttended = async (req: Request, res: Response) => {
+    try {
+        const message = await store.showAttended();
+        res.status(200).json(message);
+    } catch (error){
+        res.status(400).json(error);
+    }
+}
+
+const showNoContact = async (req: Request, res: Response) => {
+    try {
+        const message = await store.showNoContact();
+        res.status(200).json(message);
+    } catch (error){
+        res.status(400).json(error);
+    }
+}
+
+const create = async (req: Request, res: Response) => {
+    try {
+        const message_user: Message_users = {
+            message_id: req.body.message_id,
+            status_id: req.body.status_id,
+            users_id: req.body.users_id
+        }
+        const newMessage_users = await store.create(message_user);
+        res.status(201).json(newMessage_users);
+    } catch (error) {
+        res.status(400).json(error)
+    }
+}
+
+const edit = async (req: Request, res: Response) => {
+    try {
+        const message_user: Message_users = {
+            message_users_id: req.body.message_users_id,
+            // message_id: req.body.message_id,
+            status_id: req.body.status_id,
+            users_id: req.body.users_id
+        }
+        const newStatus = await store.edit(message_user);
+        res.status(200).json(newStatus);
+    } catch (error) {
+        res.status(400).json(error)
+    }
+}
+
+const message_usersRoutes = (app: express.Application): void => {
+    app.get('/messageusers', verifyAuthToken, index); // Muestra todos los mensajes modificados por su status
+    app.get('/messageusers/:message_users', verifyAuthToken, show);
+    app.get('/message-users/showfirstattempt', verifyAuthToken, showFirstAttempt); //Primer intento de contacto con el paciente
+    app.get('/message-users/showsecondattempt', verifyAuthToken, showSecondAttempt); //Segundo intento de contacto con el paciente
+    app.get('/message-users/showinprocess', verifyAuthToken, showInProcess); //Mensaje en proceso de atención
+    app.get('/message-users/showattended', verifyAuthToken, showAttended); //Mensaje atendido
+    app.get('/message-users/shownocontact', verifyAuthToken, showNoContact); //No se logró tener contacto con el paciente
+    app.post('/message-users/edit', verifyAuthToken, edit); //Nos ayuda a modificar el status de cada paciente
+    app.post('/message-users', verifyAuthToken, create);
+}
+
+export default message_usersRoutes;
---
+++ src/server/models/message_users.ts
@@ -0,0 +1,132 @@
+import Client from '../database';
+
+export type Message_users = {
+    message_users_id?: number;
+    message_id?: number;
+    status_id: number;
+    fecha_cambio?: Date;
+    users_id: number;
+}
+
+export class Message_usersStore {
+    async index(): Promise<Message_users[]>{
+        try {
+            const conn = await Client.connect();
+            const sql = 'SELECT m.firstname, m.lastname, m.phone, m.email, pa.possible_appt , m.message, m.fecha_mensaje, ms.status, u.username FROM message_users AS mu INNER JOIN messages AS m ON mu.message_id=m.message_id INNER JOIN possible_appts AS pa ON m.possible_appt=pa.possible_appt_id INNER JOIN message_status AS ms ON mu.status_id=ms.status_id INNER JOIN users AS u ON mu.users_id=u.users_id';
+            const result = await conn.query(sql);
+            conn.release();
+            return result.rows;
+        } catch (error) {
+            throw new Error(`Cannot get Messages. ${error}`);
+            
+        }
+    }
+
+    async show(message_users_id:string):Promise<Message_users>{
+        try {
+            const sql = 'SELECT * FROM message_users WHERE message_users_id=$1';
+            const conn = await Client.connect();
+            const result = await conn.query(sql, [message_users_id]);
+            conn.release();
+            return result.rows[0];
+        } catch (error) {
+            throw new Error(`Could not find message ${error}`);
+        }
+    }
+
+    async showFirstAttempt():Promise<Message_users[]>{
+        try {
+            const sql = 'SELECT m.firstname, m.lastname, m.phone, m.email, pa.possible_appt, m.message, m.fecha_mensaje, ms.status, u.username FROM message_users AS mu INNER JOIN messages AS m ON mu.message_id=m.message_id INNER JOIN message_status AS ms ON mu.status_id=ms.status_id INNER JOIN users AS u ON mu.users_id=u.users_id INNER JOIN possible_appts AS pa ON m.possible_appt=pa.possible_appt_id AND mu.status_id=2';
+            const conn = await Client.connect();
+            const result = await conn.query(sql);
+            conn.release();
+            return result.rows;
+        } catch (error) {
+            throw new Error(`Could not find the message ${error}`);
+        }
+    }
+
+    async showSecondAttempt():Promise<Message_users[]>{
+        try {
+            const sql = 'SELECT m.firstname, m.lastname, m.phone, m.email, pa.possible_appt, m.message, m.fecha_mensaje, ms.status, u.username FROM message_users AS mu INNER JOIN messages AS m ON mu.message_id=m.message_id INNER JOIN message_status AS ms ON mu.status_id=ms.status_id INNER JOIN users AS u ON mu.users_id=u.users_id INNER JOIN possible_appts AS pa ON m.possible_appt=pa.possible_appt_id AND mu.status_id=3';
+            const conn = await Client.connect();
+            const result = await conn.query(sql);
+            conn.release();
+            return result.rows;
+        } catch (error) {
+            throw new Error(`Could not find the message ${error}`);
+        }
+    }
+
+    async showNoContact():Promise<Message_users[]>{
+        try {
+            const sql = 'SELECT m.firstname, m.lastname, m.phone, m.email, pa.possible_appt, m.message, m.fecha_mensaje, ms.status, u.username FROM message_users AS mu INNER JOIN messages AS m ON mu.message_id=m.message_id INNER JOIN message_status AS ms ON mu.status_id=ms.status_id INNER JOIN users AS u ON mu.users_id=u.users_id INNER JOIN possible_appts AS pa ON m.possible_appt=pa.possible_appt_id AND mu.status_id=4';
+            const conn = await Client.connect();
+            const result = await conn.query(sql);
+            conn.release();
+            return result.rows;
+        } catch (error) {
+            throw new Error(`Could not find the message ${error}`);
+        }
+    }
+
+    async showInProcess():Promise<Message_users[]>{
+        try {
+            const conn = await Client.connect();
+            const sql = 'SELECT m.firstname, m.lastname, m.phone, m.email, pa.possible_appt, m.message, m.fecha_mensaje, ms.status, u.username FROM message_users AS mu INNER JOIN messages AS m ON mu.message_id=m.message_id INNER JOIN message_status AS ms ON mu.status_id=ms.status_id INNER JOIN users AS u ON mu.users_id=u.users_id INNER JOIN possible_appts AS pa ON m.possible_appt=pa.possible_appt_id AND mu.status_id=5';
+            const result = await conn.query(sql);
+            conn.release();
+            return result.rows;
+        } catch (error) {
+            throw new Error(`Could not find the message ${error}`);
+        }
+    }
+
+    async showAttended():Promise<Message_users[]>{
+        try {
+            const conn = await Client.connect();
+            const sql = 'SELECT m.firstname, m.lastname, m.phone, m.email, pa.possible_appt, m.message, m.fecha_mensaje, ms.status, u.username FROM message_users AS mu INNER JOIN messages AS m ON mu.message_id=m.message_id INNER JOIN message_status AS ms ON mu.status_id=ms.status_id INNER JOIN users AS u ON mu.users_id=u.users_id INNER JOIN possible_appts AS pa ON m.possible_appt=pa.possible_appt_id AND mu.status_id=6';
+            const result = await conn.query(sql);
+            conn.release()
+            return result.rows;
+        } catch (error) {
+            throw new Error(`Cannot get messages ${error}`);
+        };
+    }
+
+    async create(m: Message_users):Promise<Message_users>{
+        try {
+            const conn = await Client.connect();
+            const sql = 'INSERT INTO message_users(message_id, status_id, users_id) VALUES ($1, $2, $3) RETURNING *';
+            const result = await conn.query(sql, [
+                m.message_id,
+                m.status_id,
+                m.users_id
+            ]); 
+            const message_users = result.rows[0];
+            conn.release();
+            return message_users;
+        } catch (error) {
+            throw new Error(`Cannot create the message ${error}`);
+            
+        }
+    }
+
+    async edit(m: Message_users):Promise<Message_users>{
+        try {
+            const sql = 'UPDATE message_users SET status_id=$2, users_id=$3 WHERE message_users_id=$1';
+            const conn = await Client.connect();
+            const result = await conn.query(sql, [
+                m.message_users_id,
+                m.status_id,
+                m.users_id
+            ]);
+            const message_user = result.rows[0]
+            conn.release();
+            return message_user;
+        } catch (error) {
+            throw new Error(`Could not find user ${error}`);
+        }
+    }
+    
+}
