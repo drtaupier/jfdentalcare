01828918191b08733ac31033c73644ceb9e91783
David Rivera <drtaupier@gmail.com>
2022-10-15 17:16:48 -0400

    Se elimino el estado del mensaje

---
+++ migrations/sqls/20221015152003-messages-up.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS messages;
+DROP SEQUENCE IF EXISTS messages_message_id_seq;
+
+CREATE TABLE messages (
+    message_id SERIAL PRIMARY KEY,
+    firstname VARCHAR(50) NOT NULL,
+    lastname VARCHAR(50) NOT NULL,
+    phone VARCHAR(10) NOT NULL,
+    email VARCHAR(60),
+    possible_appt INT REFERENCES possible_appts(possible_appt_id),
+    message VARCHAR(200),
+    fecha_mensaje DATE DEFAULT NOW()
+);
